2075fd462a19e1e4fadae05b19fbffe09f1d73c0
Jonathan Leibovich <leibo023@umn.edu>
2020-02-28 14:55:39 -0600

    I don't know how to push from commandline

--- BattleBoats.java
+++ BattleBoats.java
@@ -3,6 +3,7 @@ public class BattleBoats{
   public static void main(String[] args){
     System.out.println("Hello World!");
     System.out.println("Hello World, again.");
+    System.out.println("push through git?");
 
   }
 }
