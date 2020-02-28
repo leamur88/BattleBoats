4194ccf982c6bc48e40cbef2cd2e608fffe0973b
Jonathan Leibovich <leibo023@umn.edu>
2020-02-28 14:44:09 -0600

    You can save in sublime and it'll autosave on atom

--- BattleBoats.java
+++ BattleBoats.java
@@ -1,6 +1,8 @@
 public class BattleBoats{
 
   public static void main(String[] args){
-    System.out.println("Hello World!")
+    System.out.println("Hello World!");
+    System.out.println("Hello World, again.");
+
   }
 }
