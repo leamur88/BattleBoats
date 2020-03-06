ef9fcd300ec3c9111b296649f6ad7d11c22cad38
ethanosmundson <57687169+ethanosmundson@users.noreply.github.com>
2020-03-05 18:03:19 -0600

    Test2

--- BattleBoats.java
+++ BattleBoats.java
@@ -5,7 +5,7 @@ public class BattleBoats{
     System.out.println("Hello World, again.");
     System.out.println("push through git?");
     System.out.println("Test");
-    System.out.println("yes");
+    System.out.println("Test2");
 
   }
 }
