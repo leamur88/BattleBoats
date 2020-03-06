7faecde5700e4a566de28689ee98f74ef081c90e
ethanosmundson <57687169+ethanosmundson@users.noreply.github.com>
2020-03-05 19:15:10 -0600

    toString and hitBoat

--- Boats.java
+++ Boats.java
@@ -2,6 +2,8 @@ public class Boats{
     private int length;
     private int health;
 
+    static Boats hitBoat = new Boats(-1);
+
     public Boats(int length) {
         this.health = length;
         this.length = length;
@@ -13,5 +15,18 @@ public class Boats{
 
     public void loseHealth(){
       this.health --;
+    }
+
+    public String toString(){
+        String result = "";
+        
+        if (this.health > 0) {
+            result = String.valueOf(this.health);
+        }
+
+        else if (this.health < 0) {
+            result = "X";
+        }
+    return result;
     }
 }
