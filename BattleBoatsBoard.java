60f593eec47a1cfb63b645f844d0c42deba296d8
Jonathan Leibovich <leibo023@umn.edu>
2020-03-16 15:57:55 -0500

    Yes

--- BattleBoatsBoard.java
+++ BattleBoatsBoard.java
@@ -320,9 +320,7 @@ public class BattleBoatsBoard {
 
 		}
 
-<<<<<<< HEAD
-		public void drone(int direction, int index) {  //Scans a row or column. Updates the user on how many boat objects are in the col/row
-=======
+
 		public int drone(String direction, int index) {  //Scans a row or column. Updates the s
 			int count = 0;
 			
@@ -333,7 +331,7 @@ public class BattleBoatsBoard {
 					}
 				}
 			}
->>>>>>> 0579639956a1f3496f6f0d38ffab6e6084c0980e
+
 
 			else if (direction.equals("column")){
 				for (int i = 0; i < this.board[0].length; i++){
@@ -355,21 +353,14 @@ public class BattleBoatsBoard {
 			// newboard.placeBoats();
 
 			// System.out.println(newboard);
-<<<<<<< HEAD
 
 			thisboard.missile(4,4);
 			
 			
 			
 
-
-			
-=======
-			
-			//newboard.fire(1,1);
-
 			System.out.println(thisboard.drone("column", 1));
-
->>>>>>> 0579639956a1f3496f6f0d38ffab6e6084c0980e
 		}
+
 }
+
